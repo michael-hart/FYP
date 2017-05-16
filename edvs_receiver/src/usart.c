6e9b366fcd79e18c22ef2ab1b9eaf5790cc5b36d
Michael Hart <michael.hart1994@gmail.com>
2017-05-16 17:02:33 +0100

    Switched PC comms to USART2 and remove comments

--- edvs_receiver/src/usart.c
+++ edvs_receiver/src/usart.c
@@ -67,8 +67,6 @@ void USART_Config(void)
 
 void USART_SendByte(uint8_t data)
 {
-//    xQueueSend(usart_txq, &data, 100/portTICK_RATE_MS);
-//    xQueueSend(usart_txq, &data, 100);
     xQueueSend(usart_txq, &data, portMAX_DELAY);
 }
 
@@ -80,18 +78,15 @@ void USART_SendString(char * str)
     }
 }
 
-void USART1_IRQHandler(void)
+void USART2_IRQHandler(void)
 {
     uint8_t data;
     static portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
 
-    if (USART_GetITStatus(USART1, USART_IT_RXNE)==SET) {
-        data = USART_ReceiveData(USART1);
+    if (USART_GetITStatus(USART2, USART_IT_RXNE)==SET) {
+        data = USART_ReceiveData(USART2);
         xQueueSendFromISR(usart_rxq, &data, &xHigherPriorityTaskWoken);
-    //  if (xHigherPriorityTaskWoken==pdTRUE)
-    //      portSWITCH_CONTEXT();
-
-        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+        USART_ClearITPendingBit(USART2, USART_IT_RXNE);
     }
 }
 
@@ -104,21 +99,21 @@ static void hal_init(void)
     GPIO_InitTypeDef port_init;
     USART_InitTypeDef usart_init;
   
-    //GPIO init: USART1 PA9 as OUT, PA10 as IN
+    //GPIO init: USART2 PA2 as OUT, PA3 as IN
     RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOA, ENABLE );
     
-    port_init.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    port_init.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
     port_init.GPIO_Mode = GPIO_Mode_AF;
     port_init.GPIO_Speed = GPIO_Speed_2MHz;
     port_init.GPIO_OType = GPIO_OType_PP;
     port_init.GPIO_PuPd = GPIO_PuPd_UP;
     GPIO_Init( GPIOA, &port_init );
-    GPIO_PinAFConfig(GPIOA,  GPIO_PinSource9, GPIO_AF_1);
-    GPIO_PinAFConfig(GPIOA,  GPIO_PinSource10, GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOA,  GPIO_PinSource3, GPIO_AF_1);
+    GPIO_PinAFConfig(GPIOA,  GPIO_PinSource2, GPIO_AF_1);
 
 
-    //USART init: USART1 3M 8n1
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    //USART init: USART2 500K 8n1
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
     usart_init.USART_BaudRate = USART_BAUD_RATE;
     usart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     usart_init.USART_Parity = USART_Parity_No;
@@ -126,8 +121,8 @@ static void hal_init(void)
     usart_init.USART_WordLength = USART_WordLength_8b;
     usart_init.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-    USART_Init(USART1, (USART_InitTypeDef*) &usart_init);
-    USART_Cmd(USART1, ENABLE);
+    USART_Init(USART2, (USART_InitTypeDef*) &usart_init);
+    USART_Cmd(USART2, ENABLE);
 
 }
 
@@ -135,13 +130,13 @@ static void irq_init(void)
 {
     NVIC_InitTypeDef nvic;
 
-    nvic.NVIC_IRQChannel = USART1_IRQn;
+    nvic.NVIC_IRQChannel = USART2_IRQn;
     nvic.NVIC_IRQChannelPriority = 3;
     nvic.NVIC_IRQChannelCmd = ENABLE;
 
     NVIC_Init(&nvic);
 
-    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
 
 }
 
@@ -159,13 +154,11 @@ static void usart_tx_task(void *pvParameters)
     uint8_t data;
     for (;;) {
         
-        if (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET) {
+        if (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET) {
             vTaskDelay(1);
         } else {
-            GPIO_ResetBits(GPIOC, GPIO_Pin_8);
             if (pdPASS == xQueueReceive(usart_txq, &data, portMAX_DELAY)) {
-                GPIO_SetBits(GPIOC, GPIO_Pin_8);
-                USART_SendData(USART1, data);
+                USART_SendData(USART2, data);
             }
         }
     }

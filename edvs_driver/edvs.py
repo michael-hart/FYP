b391af4cc6e361ff5b8c46f14d805321ebf85f48
Michael Hart <michael.hart1994@gmail.com>
2017-06-17 13:27:56 +0100

    Changed baud rate to 500K and updated to be compatible with python3

--- edvs_driver/edvs.py
+++ edvs_driver/edvs.py
@@ -2,27 +2,34 @@
 
 import binascii
 from enum import Enum
-from itertools import izip_longest
-import logging
 import os
-import serial
+import sys
 import time
-from Queue import Queue
+import logging
+import serial
 from serial.threaded import LineReader, ReaderThread
+from serial.tools import list_ports
 import yaml
+if sys.version_info[0] == 2:
+    from itertools import izip_longest as zip_longest
+    from Queue import Queue
+elif sys.version_info[0] == 3:
+    from queue import Queue
 
 
 EDVS_CONFIG = os.path.join(os.path.split(__file__)[0], "edvs_config.yaml")
 
 
 class EDVSState(Enum):
+    """DVS state is either sending events or awaiting commands"""
     COMMAND = 0
     EVENT = 1
 
 
 def grouper(iterable, n, fillvalue=None):
+    """Helper method to group list into groups of size n"""
     args = [iter(iterable)] * n
-    return izip_longest(*args, fillvalue=fillvalue)
+    return zip_longest(*args, fillvalue=fillvalue)
 
 
 def decode_events(raw, evt_fmt):
@@ -74,8 +81,11 @@ class DVSReader(LineReader):
     def __init__(self, *args, **kwargs):
         self._log = logging.getLogger("DVSReader")
         super(DVSReader, self).__init__(*args, **kwargs)
-        self.edvs_config = yaml.load(file(EDVS_CONFIG))
-        self.TERMINATOR = b'\n'
+        self.edvs_config = None
+        with open(EDVS_CONFIG) as conf:
+            self.edvs_config = yaml.load(conf)
+        self.terminator = b'\n'
+        self.serial = None
 
     def connection_made(self, transport):
         super(DVSReader, self).connection_made(transport)
@@ -88,29 +98,29 @@ class DVSReader(LineReader):
         self.set_event_sending(False)
 
     def handle_line(self, data):
-        self._log.debug(">>> %s :: RAW %s" %
-                        (data, binascii.hexlify(data.encode('utf-8'))))
+        self._log.debug(">>> %s :: RAW %s", data, 
+                        binascii.hexlify(data.encode('utf-8')))
         if self.state == EDVSState.EVENT:
             # Attempt to decode as event data
             data = self.buf + data
             event_data, self.buf = decode_events(data,
                                                  self.edvs_config['event_bytes'])
             if event_data != []:
-                self._log.debug("Events decoded: {}".format(event_data))
+                self._log.debug("Events decoded: %s", event_data)
                 for listener in self.listeners:
                     listener(event_data)
 
     def write_line(self, data):
-        self._log.debug("<<< %s :: RAW %s" %
-                        (data, binascii.hexlify(data.encode('utf-8'))))
+        self._log.debug("<<< %s :: RAW %s", data, 
+                        binascii.hexlify(data.encode('utf-8')))
         super(DVSReader, self).write_line(data)
 
     def connection_lost(self, exc):
         if exc:
             self._log.exception(exc)
-        with open('edvs_config.yaml', 'w') as f:
-            f.write("# Default configuration for EDVS\n")
-            yaml.dump(self.edvs_config, f, default_flow_style=False)
+        with open('edvs_config.yaml', 'w') as conf:
+            conf.write("# Default configuration for EDVS\n")
+            yaml.dump(self.edvs_config, conf, default_flow_style=False)
         self._log.info("Port closed")
 
     def add_event_listener(self, listener):
@@ -118,14 +128,14 @@ class DVSReader(LineReader):
 
     def led_set(self, led_state):
         """Sets LED state to the given number"""
-        assert type(led_state) == int
+        assert isinstance(led_state, int)
         assert 0 <= led_state <= 3
         self.write_line("!L{}".format(led_state))
 
     def set_baud_rate(self, rate):
         """Sets baud rate to given rate"""
-        assert type(rate) == int
-        self._log.info("Setting baud rate to %d" % rate)
+        assert isinstance(rate, int)
+        self._log.info("Setting baud rate to %d", rate)
         self.write_line("!U={}".format(rate))
         self.edvs_config['baud'] = rate
         self.serial.baudrate = rate
@@ -141,8 +151,8 @@ class DVSReader(LineReader):
 
     def set_event_bytes(self, _bytes):
         """Sets format for number of bytes"""
-        assert type(_bytes) == int
-        self._log.info("Setting event bytes format to {}".format(_bytes))
+        assert isinstance(_bytes, int)
+        self._log.info("Setting event bytes format to %s", _bytes)
         self.write_line("!E{}".format(_bytes))
         self.edvs_config['event_bytes'] = _bytes
 
@@ -154,44 +164,39 @@ class DVSReaderThread(ReaderThread):
         self._log = logging.getLogger("DVSReaderThread")
 
         # Get list of valid COM ports
-        valid_ports = self.list_ports()
+        valid_ports = list_ports.comports()
         # Raise exception if too few ports exist
         if len(valid_ports) < 2:
             raise DVSException("Too few COM ports")
-        ser = serial.Serial(valid_ports[1], parity=serial.PARITY_NONE,
-                            stopbits=serial.STOPBITS_ONE, rtscts=True)
+
+        # Temporarily just try and open COM5
+        ser = serial.Serial("COM5", rtscts=True)
+
+        # ser = serial.Serial(valid_ports[1].device, parity=serial.PARITY_NONE,
+                            # stopbits=serial.STOPBITS_ONE, rtscts=True)
         super(DVSReaderThread, self).__init__(ser, *args, **kwargs)
 
-    def list_ports(self):
-        """List the valid COM ports; currently Windows only"""
-        # Get valid COM ports in list
-        valid = []
-        for i in range(256):
-            try:
-                port = "COM{}".format(i)
-                s = serial.Serial(port)
-                s.close()
-                self._log.debug("Valid COM port found: %s", port)
-                valid += [port]
-            except (OSError, serial.SerialException):
-                pass
-        return valid
 
-
-if __name__ == '__main__':
+def main():
+    """Entry point of application with simple use case"""
     from loggers import init_loggers
     init_loggers()
     logger = logging.getLogger("Main")
+    logger.info("Logging starts here")
     with DVSReaderThread(DVSReader) as dvs:
-        request_baud = 115200
+        request_baud = 500000
         if dvs.edvs_config['baud'] != request_baud:
             dvs.set_baud_rate(request_baud)
         dvs.set_event_sending(True)
 
         def event_listener(data):
+            """Print event data in listener"""
             for event in data:
-                print "x:{} y:{} polarity:{}".format(
-                    event[0], event[1], event[2])
+                print("x:{} y:{} polarity:{}".format(
+                    event[0], event[1], event[2]))
         dvs.add_event_listener(event_listener)
         print("Listening for ten seconds")
         time.sleep(10)
+
+if __name__ == '__main__':
+    main()
